2cd1bb8b6494c06b624c6c4bbf37efe4bcf61ebf
sriram <sriram@FULL624-PRO.local>
2021-02-17 19:12:12 +0530

    updated path

--- src/main/java/com/books/bookinfoservice/resources/BookInfoResource.java
+++ src/main/java/com/books/bookinfoservice/resources/BookInfoResource.java
@@ -1,12 +1,12 @@
 package com.books.bookinfoservice.resources;
 
 import com.books.bookinfoservice.models.Book;
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("info")
+@RequestMapping("info")
 public class BookInfoResource {
     @RequestMapping("/")
     public Book getBookInfo() {
