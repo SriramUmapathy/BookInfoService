327089508cfeb74e96582b8fe95e8c673879e823
sriram <sriram@FULL624-PRO.local>
2021-02-19 22:41:54 +0530

    added logs and instance info

--- src/main/java/com/books/bookinfoservice/resources/BookInfoResource.java
+++ src/main/java/com/books/bookinfoservice/resources/BookInfoResource.java
@@ -1,6 +1,10 @@
 package com.books.bookinfoservice.resources;
 
 import com.books.bookinfoservice.models.Book;
+import com.books.bookinfoservice.util.InstanceInfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("info")
 public class BookInfoResource {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BookInfoResource.class);
+
+    @Autowired
+    InstanceInfoService instanceInfoService;
+
     @RequestMapping("/{bookId}")
     public Book getBookInfo(@PathVariable("bookId") String bookId) {
-        return Book.builder().bookId(bookId).name("java development").build();
+        LOGGER.info(bookId);
+
+        return Book.builder().bookId(bookId).name(instanceInfoService.retrieveInstanceInfo()).build();
     }
 }
