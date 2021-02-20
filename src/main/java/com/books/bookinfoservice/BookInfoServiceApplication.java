470e86932f5315ae6a8b5453e9d076ba023db5ea
sriram <sriram@FULL624-PRO.local>
2021-02-20 13:23:07 +0530

    log setup

--- src/main/java/com/books/bookinfoservice/BookInfoServiceApplication.java
+++ src/main/java/com/books/bookinfoservice/BookInfoServiceApplication.java
@@ -1,9 +1,9 @@
 package com.books.bookinfoservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 //@EnableEurekaClient
@@ -12,6 +12,12 @@ public class BookInfoServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookInfoServiceApplication.class, args);
 	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
 
 //	@Bean
 //	@LoadBalanced
